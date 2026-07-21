83c6dd4f8311ac411322dd4c10737fa1963472fd
Md1Haji <mhaali6363@gmail.com>
2026-07-21 17:26:48 +0530

    update folder

--- lms-platform/backend/src/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-// Centralized error handler - every controller forwards errors here via next(err)
-module.exports = function errorHandler(err, req, res, next) {
-  console.error(err.stack);
-  const statusCode = err.statusCode || 500;
-  res.status(statusCode).json({
-    message: err.message || "Internal server error",
-    ...(process.env.NODE_ENV !== "production" && { stack: err.stack }),
-  });
-};
